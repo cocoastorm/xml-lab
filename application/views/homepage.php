e006873f00f80fc9f8f8f6b1c2f2e5b1893a4e0a
Johnny <johnnymarin2011@gmail.com>
2016-03-21 14:32:44 -0700

    Made tables for categories

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -8,21 +8,118 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <title>Home</title>
 </head>
 <body>
-    <table>
+    <table width="100%" border="1">
         {days}
         <tr>
-            <th>
+            <th colspan="4" bgcolor="#9acd32">
+
                 {dayofweek}
             </th>
         </tr>
         <tr>
-            {daybooking}
+            <th width="25%">Time</th>
+            <th width="25%">Course</th>
+            <th width="25%">Room</th>
+            <th width="25%">Instructor</th>
+
+        </tr>
+        {daybooking}
+
+        <tr>
             <td>
                 {time}
             </td>
-            {/daybooking}
+            <td>
+                {courseName}
+            </td>
+            <td>
+                {room}
+            </td>
+            <td>
+                {instructor}
+            </td>
         </tr>
+        {/daybooking}
+
         {/days}
     </table>
+
+<br><br>
+
+    <table width="100%" border="1">
+        {courses}
+        <tr>
+            <th colspan="4" bgcolor="#f08080">
+
+                {code}
+            </th>
+        </tr>
+        <tr>
+            <th width="25%">Week Day</th>
+            <th width="25%">Time</th>
+            <th width="25%">Room</th>
+            <th width="25%">Instructor</th>
+
+        </tr>
+        {coursebooking}
+
+        <tr>
+            <td>
+                {weekday}
+            </td>
+            <td>
+                {time}
+            </td>
+            <td>
+                {room}
+            </td>
+            <td>
+                {instructor}
+            </td>
+        </tr>
+        {/coursebooking}
+
+        {/courses}
+    </table>
+
+<br><br>
+
+    <table width="100%" border="1">
+        {periods}
+        <tr>
+            <th colspan="4" bgcolor="#add8e6">
+
+                {time}
+            </th>
+        </tr>
+        <tr>
+            <th width="25%">Week Day</th>
+            <th width="25%">Course Name</th>
+            <th width="25%">Room</th>
+            <th width="25%">Instructor</th>
+
+        </tr>
+        {periodbooking}
+
+        <tr>
+            <td>
+                {weekday}
+            </td>
+            <td>
+                {courseName}
+            </td>
+            <td>
+                {room}
+            </td>
+            <td>
+                {instructor}
+            </td>
+        </tr>
+        {/periodbooking}
+
+        {/periods}
+    </table>
+
+
 </body>
 </html>
